345d52c3b1b02ce25dfb4261c952fa53729c012a
zzh428 <624777990@qq.com>
2019-01-10 22:18:48 +0800

    add reverse

---
+++ example/reverse.c
@@ -0,0 +1,37 @@
+int printf(char* format, ...);
+char * gets (char * str);
+int main() {
+	char src[1000];
+	int i;
+	i = 0;
+	for (i = 0; i < 1000; i++) {
+		src[i] = '\0';
+	}
+	gets(src);
+	int m;
+	m = 0;
+	for (i = 0; i < 1000; i++) {
+		if (src[i] == '\0') {
+			m = i;
+			break;
+		}
+	}
+	if (m == 0) {
+		printf("empty string");
+		return 0;
+	}
+	int flag = 1;
+	for (i = 0; i < m; i++) {
+		if (src[i] != src[m - 1 - i]) {
+			flag = 0;
+			break;
+		}
+	}
+	if (flag == 0) {
+		printf("False");
+	}
+	else {
+		printf("True");
+	}
+	return 0;
+}
