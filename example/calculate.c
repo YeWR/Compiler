0e92b2432cef8e7e69c94f5b6944a70a38d8de4c
YeWR <ywr16@mails.tsinghua.edu.cn>
2019-01-12 18:43:49 +0800

    change some c codes

--- example/calculate.c
+++ example/calculate.c
@@ -1,11 +1,12 @@
-int printf(char* format, ...);
-char * gets (char * str);
 char src[1000];
 int len;
 char operate[1000];
 int operate_top;
 int number[1000];
 int number_top;
+
+int printf(char* format, ...);
+char * gets (char * str);
 
 int isOperate(char s) {
 	if (s == '+' || s == '-' || s == '*' || s == '/') {
@@ -40,47 +41,51 @@ int cal() {
 	res = 0;
 	int num;
 	num = 0;
-	for (i = 0; i < len; i=i+1) {
+	for (i = 0; i < len; i = i + 1) {
 		if (isOperate(src[i]) || isPart(src[i])) {
 			if (isOperate(src[i])) {
 				if (operate_top == -1) {
 					operate[operate_top + 1] = src[i];
-					operate_top=operate_top+1;
+					operate_top = operate_top + 1;
 				}
 				else {
 					if (isPrior(src[i], operate[operate_top]) == 1) {
 						operate[operate_top + 1] = src[i];
-						operate_top=operate_top+1;
+						operate_top = operate_top + 1;
 					}
 					else {
-						if (operate[operate_top] == '+') {
-							number[number_top - 1] = number[number_top] + number[number_top - 1];
+						while (operate_top >= 0 && isPrior(src[i],operate[operate_top]) == 0) {
+							if (operate[operate_top] == '+') {
+								number[number_top - 1] = number[number_top] + number[number_top - 1];
+							}
+							if (operate[operate_top] == '-') {
+								number[number_top - 1] = number[number_top - 1] - number[number_top];
+							}
+							if (operate[operate_top] == '*') {
+								number[number_top - 1] = number[number_top] * number[number_top - 1];
+							}
+							if (operate[operate_top] == '/') {
+								number[number_top - 1] = number[number_top - 1] / number[number_top];
+							}
+							if (operate[operate_top] == '(') {
+								operate[operate_top + 1] = src[i];
+								operate_top = operate_top + 1;
+								continue;
+							}
+							if (operate[operate_top] != '(') {
+								number_top = number_top - 1;
+								operate_top = operate_top - 1;
+							}
 						}
-						if (operate[operate_top] == '-') {
-							number[number_top - 1] = number[number_top - 1] - number[number_top];
-						}
-						if (operate[operate_top] == '*') {
-							number[number_top - 1] = number[number_top] * number[number_top - 1];
-						}
-						if (operate[operate_top] == '/') {
-							number[number_top - 1] = number[number_top - 1] / number[number_top];
-						}
-						if (operate[operate_top] == '(') {
-							operate[operate_top + 1] = src[i];
-							operate_top=operate_top+1;
-							continue;
-						}
-						if (operate[operate_top] != '(') {
-							number_top = number_top - 1;
-							operate[operate_top] = src[i];
-						}
+						operate_top = operate_top + 1;
+						operate[operate_top] = src[i];
 					}
 				}
 			}
 			if (isPart(src[i])) {
 				if (src[i] == '(') {
 					operate[operate_top + 1] = src[i];
-					operate_top=operate_top+1;
+					operate_top = operate_top + 1;
 				}
 				else {
 					while (operate[operate_top] != '(') {
@@ -106,9 +111,9 @@ int cal() {
 		else {
 			num = num * 10;
 			num = num + src[i] - '0';
-			if (i == len - 1 || (i != len-1 && (isOperate(src[i+1]) || isPart(src[i+1])))) {
+			if (i == len - 1 || (i != len - 1 && (isOperate(src[i + 1]) || isPart(src[i + 1])))) {
 				number[number_top + 1] = num;
-				number_top = number_top+1;
+				number_top = number_top + 1;
 				num = 0;
 			}
 		}
@@ -139,13 +144,13 @@ int main() {
 	number_top = -1;
 	int i;
 	i = 0;
-	for (i = 0; i < 1000; i=i+1) {
+	for (i = 0; i < 1000; i = i + 1) {
 		src[i] = '\0';
 		operate[i] = '\0';
 		number[i] = 0;
 	}
 	gets(src);
-	for (i = 0; i < 1000; i=i+1) {
+	for (i = 0; i < 1000; i = i + 1) {
 		if (src[i] == '\0') {
 			len = i;
 			break;
